41cd8e37fdf0681c9e4f8f536e0e40e982e151a7
deimar <deimar.parn@khk.ee>
2022-03-10 16:22:56 +0000

    Added chat function with react hooks

--- Users/deimar/Desktop/voco/src/Chat.jsx
+++ Users/deimar/Desktop/voco/src/Chat.jsx
@@ -1,30 +1,62 @@
-function Chat() {
-    return (
-<div class="box">
-  <form>
-  <div class="container">
-      <input type="text" name="username" class="user" placeholder="Username" />
-    </div>
-    <div class="container">
-      <input type="text" name="message" class="form-control" placeholder="Message" />
-    </div>
-    <div>
-            <button type="submit" class="btn">SEND</button>
-        </div>
-  </form>
-  <ul> 
-    <div class="min-box">
-<li class="listing">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-<p> John Smith * 12.01.2022 15:32</p></li>
-<li class="listing">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-<p> John Smith * 12.01.2022 15:32</p></li>
-<li  class="listing">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-<p> John Smith * 12.01.2022 15:32</p></li>
+import {useState} from "react";
 
-    </div>
-  </ul>
-</div>
-  );
-  }
 
-  export default Chat;
+        function App ()  {
+        const [author, setAuthor] = useState('');
+        const [submittedText, setSubmittedText] = useState(null);
+        const [message, setMessage] = useState('');
+        const [submittedText2, setSubmittedText2] = useState(null);
+        const textChangeHandler = (i) => {
+            setAuthor(i.target.value);
+            //console.log(i.target.value);
+
+        };
+
+        const textChangeHandler2 = (i) => {
+            setMessage(i.target.value);
+            //console.log(i.target.value);
+
+        };
+
+        const submitHandler = (event) => {
+            event.preventDefault();
+            setSubmittedText(author);
+            setAuthor("");
+
+            setSubmittedText2(message);
+            setMessage("");
+        };
+
+        return (
+            <div className="App">
+                <h1 class="title">MYCHAT</h1>
+                <form onSubmit={submitHandler}>
+                    <input class="user"
+                        placeholder="Username"
+                        type="text"
+                        value={author}
+                        onChange={textChangeHandler}
+                    />
+
+                    <input class="message"
+                        placeholder="Message"
+                        type="text"
+                        value={message}
+                        onChange={textChangeHandler2}
+                    />
+
+                    <button class="btn" type="submit" >
+                        Submit
+                    </button>
+                </form>
+                <div class="text">
+                {submittedText && (<p  id="text-author"> User Name:&nbsp;&nbsp;{submittedText}</p>)}
+                {submittedText2 && (<p id="text-message">{submittedText2}</p>)}
+            </div>
+            </div>
+        );
+    }
+export default App;
+
+
+
